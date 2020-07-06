d7b333ec9d9eca92abcb9adb6fb81a74d8b1ef0b
Filippo Maria Raeli <filippo.raeli@gmail.com>
2020-07-06 09:56:11 +0200

    Update layers.js

--- ComuniPisa/js/a_quadroConoscitivo/layers.js
+++ ComuniPisa/js/a_quadroConoscitivo/layers.js
@@ -71,7 +71,7 @@ function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        click: zoomToFeature
+        click: highlightFeature
     });
 }
 
